d9b2f39eb190b4b961f4d9a4c48b0509eb095a13
Musab Abdelrahman <40957255+mosab9@users.noreply.github.com>
2023-11-28 13:33:19 +0000

    update
    update

--- simpledl-backend/src/main/java/com/simpledl/backend/WebMvcConfig.java
+++ simpledl-backend/src/main/java/com/simpledl/backend/WebMvcConfig.java
@@ -21,6 +21,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000") // Update with your frontend URL
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*");
     }
