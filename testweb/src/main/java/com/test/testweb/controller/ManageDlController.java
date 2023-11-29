b46f50393803dc3449497865346ac2e769784576
Musab Abdelrahman <40957255+mosab9@users.noreply.github.com>
2023-11-29 18:58:07 +0000

    add test code
    add test code

---
+++ testweb/src/main/java/com/test/testweb/controller/ManageDlController.java
@@ -0,0 +1,32 @@
+package com.test.testweb.controller;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@CrossOrigin(origins="*")
+@RequestMapping("/api/manage")
+public class ManageDlController {
+
+    @PostMapping("/create/{instanceName}")
+    public ResponseEntity<String> createNewInstance(
+            @PathVariable String instanceName,
+            @RequestParam String title,
+            @RequestParam String footer,
+            @RequestParam String backgroundColor,
+            @RequestParam("imageFile") MultipartFile imageFile) {
+        return ResponseEntity.ok("response");
+    }
+
+    @GetMapping("/instances")
+    public ResponseEntity<List<String>> getInstances() {
+        List<String> supplierNames = new ArrayList<>();
+        supplierNames.add("Musab");
+        supplierNames.add("Mustafa");
+        supplierNames.add("Ahmed");
+        return ResponseEntity.ok(supplierNames);
+    }
+
+}
