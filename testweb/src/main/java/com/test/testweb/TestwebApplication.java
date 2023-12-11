fd8c1e0897ea9cc24e54b68e3d8b1e31ab8ead4f
musab <mosabkamal@gmail.com>
2023-12-11 16:05:47 +0000

    add update

--- testweb/src/main/java/com/test/testweb/TestwebApplication.java
+++ testweb/src/main/java/com/test/testweb/TestwebApplication.java
@@ -2,6 +2,14 @@ package com.test.testweb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.Ordered;
+import java.util.Arrays;
+import java.util.Collections;
 
 @SpringBootApplication
 public class TestwebApplication {
@@ -9,5 +17,20 @@ public class TestwebApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TestwebApplication.class, args);
 	}
+/*
+	@Bean
+	public FilterRegistrationBean<CorsFilter> simpleCorsFilter() {
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		CorsConfiguration config = new CorsConfiguration();
+		config.setAllowCredentials(true);
+		config.setAllowedOrigins(Arrays.asList("http://localhost:3000","http://192.168.130.27:3000","http://127.0.0.1:3000"));
+		config.setAllowedMethods(Collections.singletonList("*"));
+		config.setAllowedHeaders(Collections.singletonList("*"));
+		source.registerCorsConfiguration("/**", config);
+		FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(new CorsFilter(source));
+		bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+
+		return bean;
+	}*/
 
 }
